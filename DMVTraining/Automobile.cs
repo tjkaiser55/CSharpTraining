bb62930a4adbba688a98051ed70c1e55cbb6dd7d
tjkaiser55 <kaistom@gmail.com>
2019-09-09 07:41:19 -0400

    Add project files.

---
+++ DMVTraining/Automobile.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMVTraining
+{
+	/// <summary>
+	/// This class is a blueprint for a custom datatype
+	/// 
+	/// </summary>
+	class Automobile
+	{
+		// member variable
+		private string hp;
+		private string make;
+		private string model;
+		private string vin;
+		private int modelYear;
+		private string vehicleName;
+		private string valueStatus;
+
+
+		public enum ValueValid
+		{
+			Valid,
+			Spaces,
+			LowValues,
+			Custom
+		}
+
+		public string ValueStatus
+		{
+			get
+			{
+				return this.valueStatus;
+			}
+			set
+			{
+				this.valueStatus = value;
+			}
+		}
+
+		public int ModelYear {
+			get
+			{
+				return this.modelYear;
+			}
+			set
+			{
+				this.modelYear = value;
+			}
+		}
+		public string Hp
+		{
+			get
+			{
+				return this.hp;
+			}
+			set
+			{
+				this.hp = value;
+			}
+		}
+
+		public string Vin
+		{
+			get
+			{
+				return this.vin;
+			}
+			set
+			{
+				this.vin = value;
+			}
+		}
+
+		public string VehicleName
+		{
+			get
+			{
+				return this.vehicleName;
+			}
+			set
+			{
+				this.vehicleName = value;
+			}
+		}
+		
+
+
+		// default constructor
+		public Automobile(string vin, string make)
+		{
+			this.vin = vin;
+			this.make = make;
+		}
+
+		public Automobile(string make, string model, string hp)
+		{
+			
+			this.make = make;
+			this.model = model;
+			this.Hp = hp;
+		}
+
+		public Automobile(string vehicleName, string make, string model, string hp)
+		{
+			this.vehicleName = vehicleName;
+			this.make = make;
+			this.model = model;
+			this.Hp = hp;
+		}
+
+		public void Honk()
+		{
+			//
+			if (!string.IsNullOrEmpty(vehicleName) && hp != null && !string.IsNullOrEmpty(make) && !string.IsNullOrEmpty(model) && !string.IsNullOrEmpty(vin))
+			{
+				Console.WriteLine("Honk from {0}. My vin is {1} and I have {2} horsepower. I am a {3} {4}.", vehicleName, vin, hp, make, model);
+			}
+			else if (vehicleName != null && !string.IsNullOrEmpty(make) && !string.IsNullOrEmpty(hp))
+			{
+				Console.WriteLine("Honk from {0}. My make is {1} and my horsepower is {2}.", vehicleName, make, hp);
+			}
+			else
+			{
+				Console.WriteLine("Honk from {0}.", make);
+			}
+
+		}
+
+		public void WriteToLog(string logEntry)
+		{
+			using (System.IO.StreamWriter writer = new System.IO.StreamWriter("Logfile.txt", true))
+			{
+				DateTime dt = DateTime.Now;
+				string todaysDate = dt.ToShortDateString();
+				writer.WriteLine("Log entry on {0}", dt.ToString());
+				writer.WriteLine(logEntry);
+			}
+
+		}
+
+
+	}
+}
---
+++ DMVTraining/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DMVTraining.Automobile;
+
+namespace DMVTraining
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			// create a new instance of automobile
+			Automobile myAuto = new Automobile("25or6to4", "Chevy");
+			myAuto.Honk();
+
+			Automobile swampBuggy = new Automobile("Betsy", "Ford", "Ranger", "350");
+			swampBuggy.Vin = "Rq234";
+			Console.WriteLine("My new swamp buggy has {0} horsepower", swampBuggy.Hp);
+			swampBuggy.Honk();
+			swampBuggy.ValueStatus = "Spaces";
+
+			
+
+			Automobile myNewCar = new Automobile("ZZ123", "BMW");
+			myNewCar.Hp = "230";
+			myNewCar.ModelYear = 2019;
+			myNewCar.Honk();
+			//Console.WriteLine("MyNewCar has {0} horsepower and the model year is {1}.", myNewCar.Hp, myNewCar.ModelYear);
+
+			//Console.WriteLine("New auto using second constructor");
+			Automobile mySpareCar = new Automobile("Toyota", "Celica", "68");
+			mySpareCar.ModelYear = 2016;
+			mySpareCar.Honk();
+			
+
+			mySpareCar.WriteToLog(String.Format("Entered a new log entry for my {0} spare car.", mySpareCar.ModelYear));
+
+			// Enums
+			DateTime dt = new DateTime(2019, 8, 14);
+			Console.WriteLine("Is Wednesday the day of the week for {0:d}?: {1}",
+							   dt, dt.DayOfWeek == DayOfWeek.Wednesday);
+			Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
+
+	Console.ReadLine();
+		}
+	}
+}
